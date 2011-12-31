369c8ebfe6b3b04a6191d7babeaa0e5584bc58b7
reyalp <reyalp@2d6b39dd-413e-4bac-91f6-06b8ab484445>
2011-12-31 20:48:24 +0000

    add a simple script for making binary releases, re-organize acknowledgments for 3rd party packages
    git-svn-id: http://subversion.assembla.com/svn/chdkptp/trunk@80 2d6b39dd-413e-4bac-91f6-06b8ab484445

---
+++ bin-snapshot.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+# build a snapshot zip 
+OSTYPE=`uname -o`
+if [ "$OSTYPE" = "Msys" ] ; then
+	OS="win32"
+	EXE=".exe"
+else 
+	OS=`uname -sm | sed -e 's/ /-/'`
+	EXE=""
+fi
+REV=`svnversion  . | sed -e 's/:/-/'`
+ZIPNAME="chdkptp-r$REV-$OS.zip"
+echo $ZIPNAME
+if [ -f "$ZIPNAME" ] ; then
+	rm -f "$ZIPNAME"
+fi
+
+PROG="chdkptp$EXE"
+make DEBUG="" clean all
+strip "$PROG"
+zip "$ZIPNAME" "$PROG" chdkptp-sample.sh lua/chdku.lua lua/cli.lua lua/gui.lua lua/main.lua lua/util.lua README USAGE.TXT COPYING THANKS.TXT
