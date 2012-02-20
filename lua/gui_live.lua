a486b066c966d4d83720adbf0251d483773a9897
reyalp <reyalp@2d6b39dd-413e-4bac-91f6-06b8ab484445>
2012-02-20 01:54:54 +0000

    live gui - move locals into table members, avoid making lots of new ustimes every frame, dump live view info to console
    git-svn-id: http://subversion.assembla.com/svn/chdkptp/trunk@167 2d6b39dd-413e-4bac-91f6-06b8ab484445

--- lua/gui_live.lua
+++ lua/gui_live.lua
@@ -17,14 +17,36 @@
 --[[
 module for live view gui
 ]]
-local m={}
+local m={
+--[[
+note - these are 'private' but exposed in the module for easier debugging
+container -- outermost widget
+livedata -- latest data fetched from cam
+icnv -- iup canvas
+vp_active -- viewport streaming selected
+bm_active -- bitmap streaming selected
+timer -- timer for fetching updates
+statslabel -- text for stats
+livehandler -- "handler" to call for live data
+livebasedata -- non-changing framebuffer data
+]]
+}
 function m.live_support()
 	return (cd ~= nil
 			and type(cd.CreateCanvas) == 'function'
 			and type(chdk.put_live_image_to_canvas ) == 'function')
 end
 
-local stats={ }
+local stats={
+	t_start_frame = ustime.new(),
+	t_end_frame = ustime.new(),
+	t_start_xfer = ustime.new(),
+	t_end_xfer = ustime.new(),
+	t_start_draw = ustime.new(),
+	t_end_draw = ustime.new(),
+	t_start = ustime.new(),
+	t_stop = ustime.new(),
+}
 function stats:init_counters()
 	self.count_xfer = 0
 	self.count_frame = 0
@@ -39,12 +61,7 @@ function stats:start()
 		return
 	end
 	self:init_counters()
-	self.t_start_xfer = nil
-	self.t_end_xfer = nil
-	self.t_start_draw = nil
-	self.t_end_draw = nil
-	self.t_start = ustime.new()
-	self.t_stop = nil
+	self.t_start:get()
 	self.run = true
 end
 function stats:stop()
@@ -52,23 +69,23 @@ function stats:stop()
 		return
 	end
 	self.run = false
-	self.t_stop = ustime.new()
+	self.t_stop:get()
 end
 
 function stats:start_frame()
-	self.t_start_frame = ustime.new()
+	self.t_start_frame:get()
 	self.count_frame = self.count_frame + 1
 end
 
 function stats:end_frame()
-	self.t_end_frame = ustime.new()
+	self.t_end_frame:get()
 end
 function stats:start_xfer()
-	self.t_start_xfer = ustime.new()
+	self.t_start_xfer:get()
 	self.count_xfer = self.count_xfer + 1
 end
 function stats:end_xfer(bytes)
-	self.t_end_xfer = ustime.new()
+	self.t_end_xfer:get()
 	self.xfer_last = bytes
 	self.xfer_total = self.xfer_total + bytes
 end
@@ -88,10 +105,8 @@ function stats:get()
 		run = "no"
 		t_end = self.t_stop
 	end
-	local tsec = 1 -- dummy to prevent div0
-	if t_end and self.t_start then
-		tsec = (t_end:diffms(self.t_start)/1000)
-	else
+	local tsec = (t_end:diffms(self.t_start)/1000)
+	if tsec == 0 then
 		tsec = 1 
 	end
 	if self.count_frame > 0 then
@@ -122,36 +137,90 @@ Xfer last kb/s: %d]],
 		bps_last/1024)
 end
 
-local container -- outermost widget
-local livedata -- latest data fetched from cam
-local icnv -- iup canvas
-local vp_active -- viewport streaming selected
-local bm_active -- bitmap streaming selected
-local timer -- timer for fetching updates
-local statslabel -- text for stats
+
 local function toggle_vp(ih,state)
-	vp_active = (state == 1)
+	m.vp_active = (state == 1)
 end
 
 local function toggle_bm(ih,state)
-	bm_active = (state == 1)
+	m.bm_active = (state == 1)
 end
 
 local function update_should_run()
 	-- TODO global maintabs
-	if not con:is_connected() or maintabs.value ~= container then
+	if not con:is_connected() or maintabs.value ~= m.container then
 		return false
 	end
-	return (vp_active or bm_active)
+	return (m.vp_active or m.bm_active)
 end
+
+-- TODO temp test, should have a proper binding in C (?)
+m.lbasedata={}
+local base_fields={
+	'version_major',
+	'version_minor',
+	'vp_max_width',
+	'vp_max_height',
+	'vp_buffer_width',
+	'bm_max_width',
+	'bm_max_height',
+	'bm_buffer_width',
+	'lcd_ascpect_ratio',
+}
+local function update_basedata(basedata)
+	local t={basedata:get_i32(0,-1)}
+	printf("update_basedata\n");
+	if #t ~= #base_fields then
+		printf("size mismatch!\n")
+	end
+	for i,f in ipairs(base_fields) do
+		m.lbasedata[f]=t[i]
+		printf("%s:%s\n",f,tostring(t[i]))
+	end
+end
+
+m.lvidinfo={}
+local vidinfo_fields={
+	'vp_xoffset',             -- Viewport X offset in pixels (for cameras with variable image size)
+	'vp_yoffset',             -- Viewport Y offset in pixels (for cameras with variable image size)
+	'vp_width',               -- Actual viewport width in pixels (for cameras with variable image size)
+	'vp_height',              -- Actual viewport height in pixels (for cameras with variable image size)
+	'vp_buffer_start',        -- Offset in data transferred where the viewport data starts
+	'vp_buffer_size',         -- Size of viewport data sent (in bytes)
+	'bm_buffer_start',        -- Offset in data transferred where the bitmap data starts
+	'bm_buffer_size',         -- Size of bitmap data sent (in bytes)
+	'palette_type',           -- Camera palette type 
+							  -- (0 = no palette', 1 = 16 x 4 byte AYUV values, 2 = 16 x 4 byte AYUV values with A = 0..3, 3 = 256 x 4 byte AYUV values with A = 0..3)
+	'palette_buffer_start',   -- Offset in data transferred where the palette data starts
+	'palette_buffer_size',    -- Size of palette data sent (in bytes)
+}
+local function update_vidinfo(livedata)
+	local dirty
+	for i,f in ipairs(vidinfo_fields) do
+		local v = livedata:get_i32((i-1)*4)
+		if v ~= m.lvidinfo[f] then
+			dirty = true
+		end
+	end
+	if dirty then
+		printf('update_vidinfo: changed\n')
+		for i,f in ipairs(vidinfo_fields) do
+			local v = livedata:get_i32((i-1)*4)
+			printf("%s:%s->%s\n",f,tostring(m.lvidinfo[f]),v)
+			m.lvidinfo[f]=v
+		end
+	end
+end
+
 
 function m.init()
 	if not m.live_support() then
 		return false
 	end
-	icnv = iup.canvas{rastersize="362x242",expand="NO"}
-	statslabel = iup.label{size="90x80",alignment="ALEFT:ATOP"}
-	container = iup.hbox{
+	local icnv = iup.canvas{rastersize="362x242",expand="NO"}
+	m.icnv = icnv
+	m.statslabel = iup.label{size="90x80",alignment="ALEFT:ATOP"}
+	m.container = iup.hbox{
 		icnv,
 		iup.vbox{
 			iup.frame{
@@ -162,7 +231,7 @@ function m.init()
 				title="Stream"
 			},
 			iup.frame{
-				statslabel,
+				m.statslabel,
 				title="Statistics",
 			},
 		},
@@ -176,14 +245,14 @@ function m.init()
 
 	function icnv:action()
 		-- TODO global from gui
-		if maintabs.value ~= container then
+		if maintabs.value ~= m.container then
 			return;
 		end
 		local ccnv = self.ccnv     -- retrieve the CD canvas from the IUP attribute
 		stats:start_frame()
 		ccnv:Activate()
-		if livedata then
-			if not chdk.put_live_image_to_canvas(ccnv,livedata) then
+		if m.livedata then
+			if not chdk.put_live_image_to_canvas(ccnv,m.livedata) then
 				print('put fail')
 			end
 		else
@@ -198,32 +267,35 @@ function m.init()
 	end
 	]]
 
-	container_title='Live'
-	timer = iup.timer{ 
+	m.container_title='Live'
+	m.timer = iup.timer{ 
 		time = "100",
 	}
-	function timer:action_cb()
+	function m.timer:action_cb()
 		if update_should_run() then
 			stats:start()
 			local what=0
-			if vp_active then
+			if m.vp_active then
 				what = 1
 			end
-			if bm_active then
+			if m.bm_active then
 				what = what + 4
 			end
 			if what == 0 then
 				return
 			end
 			-- TODO this needs to get reset on disconnect
-			if not con.live_handler then
+			if not m.livehandler then
 				--print('getting handler')
-				con.live_handler = con:get_handler(1)
+				m.livehandler = con:get_handler(1)
+				m.livebasedata = con:call_handler(m.livehandler,0x80)
+				update_basedata(m.livebasedata)
 			end
 			stats:start_xfer()
-			livedata = con:call_handler(con.live_handler,what)
-			if livedata then
-				stats:end_xfer(livedata:len())
+			m.livedata = con:call_handler(m.livehandler,what)
+			if m.livedata then
+				stats:end_xfer(m.livedata:len())
+				update_vidinfo(m.livedata)
 			else
 				stats:stop()
 			end
@@ -231,30 +303,30 @@ function m.init()
 		else
 			stats:stop()
 		end
-		statslabel.title = stats:get()
+		m.statslabel.title = stats:get()
 	end
-	m.timer = timer -- test 
 end
 function m.get_container()
-	return container
+	return m.container
 end
 function m.get_container_title()
-	return container_title
+	return m.container_title
 end
 function m.on_connect_change(lcon)
 	-- reset on connect or disconnect, will get updated in timer
-	lcon.live_handler = nil
+	m.livehandler = nil
+	m.livebasedata = nil
 end
 -- check whether we should be running, update timer
 function m.update_run_state()
-	if timer then
+	if m.timer then
 		-- TODO check this is updated in the callback for gtk
 		-- better to just check if control is visible
-		if maintabs.value == container then
-			timer.run = "YES"
+		if maintabs.value == m.container then
+			m.timer.run = "YES"
 			stats:start()
 		else
-			timer.run = "NO"
+			m.timer.run = "NO"
 			stats:stop()
 		end
 	end
