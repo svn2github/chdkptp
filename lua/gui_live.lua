7e28461be9a888bc7ea57807e067528b9fa15470
reyalp <reyalp@2d6b39dd-413e-4bac-91f6-06b8ab484445>
2012-03-04 05:43:59 +0000

    live gui - split stats into it's own file
    git-svn-id: http://subversion.assembla.com/svn/chdkptp/trunk@212 2d6b39dd-413e-4bac-91f6-06b8ab484445

--- lua/gui_live.lua
+++ lua/gui_live.lua
@@ -17,6 +17,8 @@
 --[[
 module for live view gui
 ]]
+local stats=require('gui_live_stats')
+
 local m={
 	vp_par = 2, -- pixel aspect ratio for viewport 1:n, n=1,2
 	bm_par = 1, -- pixel aspect ratio for bitmap 1:n, n=1,2
@@ -34,109 +36,6 @@ function m.live_support()
 	local caps = guisys.caps()
 	return (caps.CD and caps.LIVEVIEW)
 end
-
-local stats={
-	t_start_frame = ustime.new(),
-	t_end_frame = ustime.new(),
-	t_start_xfer = ustime.new(),
-	t_end_xfer = ustime.new(),
-	t_start_draw = ustime.new(),
-	t_end_draw = ustime.new(),
-	t_start = ustime.new(),
-	t_stop = ustime.new(),
-}
-function stats:init_counters()
-	self.count_xfer = 0
-	self.count_frame = 0
-	self.xfer_last = 0
-	self.xfer_total = 0
-end
-
-stats:init_counters()
-
-function stats:start()
-	if self.run then
-		return
-	end
-	self:init_counters()
-	self.t_start:get()
-	self.run = true
-end
-function stats:stop()
-	if not self.run then
-		return
-	end
-	self.run = false
-	self.t_stop:get()
-end
-
-function stats:start_frame()
-	self.t_start_frame:get()
-	self.count_frame = self.count_frame + 1
-end
-
-function stats:end_frame()
-	self.t_end_frame:get()
-end
-function stats:start_xfer()
-	self.t_start_xfer:get()
-	self.count_xfer = self.count_xfer + 1
-end
-function stats:end_xfer(bytes)
-	self.t_end_xfer:get()
-	self.xfer_last = bytes
-	self.xfer_total = self.xfer_total + bytes
-end
-function stats:get()
-	local run
-	local t_end
-	-- TODO a rolling average would be more useful
-	local fps_avg = 0
-	local frame_time =0
-	local bps_avg = 0
-	local xfer_time = 0
-	local bps_last = 0
-
-	if self.run then
-		run = "yes"
-		t_end = self.t_end_frame
-	else
-		run = "no"
-		t_end = self.t_stop
-	end
-	local tsec = (t_end:diffms(self.t_start)/1000)
-	if tsec == 0 then
-		tsec = 1 
-	end
-	if self.count_frame > 0 then
-		fps_avg = self.count_frame/tsec
-		frame_time = self.t_end_frame:diffms(self.t_start_frame)
-	end
-	if self.count_xfer > 0 then
-		-- note this includes sleep
-		bps_avg = self.xfer_total/tsec
-		xfer_time = self.t_end_xfer:diffms(self.t_start_xfer)
-		-- instananeous
-		bps_last = self.xfer_last/xfer_time*1000
-	end
-	-- TODO this rapidly spams lua with lots of unique strings
-	return string.format(
-[[Running: %s
-FPS avg: %0.2f
-Frame last ms: %d
-T/P avg kb/s: %d
-Xfer last ms: %d
-Xfer kb: %d
-Xfer last kb/s: %d]],
-		run,
-		fps_avg,
-		frame_time,
-		bps_avg/1024,
-		xfer_time,
-		self.xfer_last/1024,
-		bps_last/1024)
-end
-
 
 function m.get_current_frame_data()
 	if m.dump_replay then
