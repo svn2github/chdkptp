f6a0931c56f0fed52eae77e70cc811345c3f36c6
reyalp <reyalp@2d6b39dd-413e-4bac-91f6-06b8ab484445>
2012-03-18 21:11:40 +0000

    debug option for bin-snapshot
    git-svn-id: http://subversion.assembla.com/svn/chdkptp/trunk@231 2d6b39dd-413e-4bac-91f6-06b8ab484445

--- bin-snapshot.bash
+++ bin-snapshot.bash
@@ -1,5 +1,37 @@
 #!/bin/bash
 # build a snapshot zip 
+name=`basename "$0"`
+function warn {
+	echo "$name warning: $1" >&2
+}
+
+function usage {
+	[ "$1" ] && warn "$1"
+	cat >&2 <<EOF
+make a binary snapshot
+usage:
+	$name [options]
+options:
+	-debug: build debug and don't strip
+EOF
+	exit 1
+}
+
+arg="$1"
+debug=""
+while [ ! -z "$arg" ] ; do
+	case $arg in
+	-debug)
+		debug=1
+	;;
+	*)
+		usage "unknown option $arg"
+	;;
+	esac
+	shift
+	arg="$1"
+done
+
 OSTYPE=`uname -o`
 if [ "$OSTYPE" = "Msys" ] ; then
 	OS="win32"
@@ -9,15 +41,22 @@ else
 	EXE=""
 fi
 REV=`svnversion  . | sed -e 's/:/-/'`
-ZIPNAME="chdkptp-r$REV-$OS.zip"
+if [ -z "$debug" ] ; then
+	ZIPNAME="chdkptp-r$REV-$OS.zip"
+else
+	ZIPNAME="chdkptp-r$REV-$OS-dbg.zip"
+fi
+
 echo $ZIPNAME
 if [ -f "$ZIPNAME" ] ; then
 	rm -f "$ZIPNAME"
 fi
 
 PROG="chdkptp$EXE"
-make DEBUG="" clean all
-strip "$PROG"
+make DEBUG="$debug" clean all
+if [ -z "$debug" ] ; then
+	strip "$PROG"
+fi
 zip "$ZIPNAME" "$PROG" chdkptp-sample.sh \
 	lua/*.lua \
 	README.TXT USAGE.TXT COPYING THANKS.TXT
