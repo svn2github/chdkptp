0b92e2f6db1f6ffca316d6e28f6d3bda5afda30d
reyalp <reyalp@2d6b39dd-413e-4bac-91f6-06b8ab484445>
2013-05-03 19:38:57 +0000

    msgtest - make .test() do repeated, fixed length messages, change length testing mode to .testlen(), make msg_shell.msg_wait nil for sleep testing
    git-svn-id: http://subversion.assembla.com/svn/chdkptp/trunk@339 2d6b39dd-413e-4bac-91f6-06b8ab484445

--- lua/extras/msgtest.lua
+++ lua/extras/msgtest.lua
@@ -20,7 +20,7 @@ a script for stressing the usb layer and message system
 usage:
 !m=require'msgtest'
 !m.load()
-!m.test(<start lenght>,<optional max length>,<optional increment>)
+!m.testlen(<start length>,<optional max length>,<optional increment>)
 
 set m.checkmem to report memory usage
 use m.gc(<collect garbage arg>) to perform garbage collection on each iteration, either 'step', 'collect' or nil
@@ -33,6 +33,7 @@ function m.load()
 		return false,err
 	end
 	return con:write_msg([[exec
+msg_shell.msg_wait = nil
 msg_shell.default_cmd=function(msg)
 	if msgtest_gc then
 		collectgarbage(msgtest_gc)
@@ -45,7 +46,40 @@ end
 ]])
 end
 
-function m.test(s,n,inc)
+function m.test_msg(len)
+	local s=string.rep('x',len)
+	local status,err=con:write_msg(s)
+	if not status then
+		printf('send failed\n')
+		return
+	end
+	local r
+	status,r = con:wait_msg({mtype='user'})
+	if not status then
+		printf('read failed %s\n',r) 
+	elseif s == r.value then 
+		printf('ok\n')
+	else
+		printf('not equal\n')
+	end
+	if m.checkmem then
+		local status,err=con:write_msg('memstats')
+		if not status then
+			printf('send failed\n')
+			return
+		end
+		status,r = con:wait_msg({mtype='user'})
+		if not status then
+			printf('read failed %s\n',r) 
+		else
+			printf(r.value..'\n')
+		end
+	end
+end
+--[[
+test messages of increasing length, from s to n
+]]
+function m.testlen(s,n,inc)
 	if not n then 
 		n=s
 	end
@@ -53,37 +87,20 @@ function m.test(s,n,inc)
 		inc=1
 	end
 	for i=s,n,inc do 
-		local s=string.rep('x',i)
 		printf("sending %d (0x%x)...",i,i)
-		local status,err=con:write_msg(s)
-		if not status then
-			printf('send failed\n')
-			return
-		end
-		local r
-		status,r = con:wait_msg({mtype='user'})
-		if not status then
-			printf('read failed %s\n',r) 
-		elseif s == r.value then 
-			printf('ok\n')
-		else
-			printf('not equal\n')
-		end
-		if m.checkmem then
-			local status,err=con:write_msg('memstats')
-			if not status then
-				printf('send failed\n')
-				return
-			end
-			status,r = con:wait_msg({mtype='user'})
-			if not status then
-				printf('read failed %s\n',r) 
-			else
-				printf(r.value..'\n')
-			end
-		end
-	 end
+		m.test_msg(i)
+	end
 end
+--[[
+test messages size s, n times
+]]
+function m.test(s,n)
+	for i=1,n do 
+		printf("send %d...",i)
+		m.test_msg(s)
+	end
+end
+
 function m.gc(mode)
 	if not mode then
 		mode='nil'
