5e824d127a861d74a3fded0b99487d487260c192
reyalp <reyalp@2d6b39dd-413e-4bac-91f6-06b8ab484445>
2017-12-20 06:29:13 +0000

    fix readlink command in shell script wrappers
    git-svn-id: http://subversion.assembla.com/svn/chdkptp/trunk@794 2d6b39dd-413e-4bac-91f6-06b8ab484445

--- misc/chdkptp-dist.sh
+++ misc/chdkptp-dist.sh
@@ -2,7 +2,7 @@
 # shell script wrapper for self contained CLI install
 CHDKPTP_EXE=chdkptp
 # path where chdkptp is installed
-CHDKPTP_DIR="$(dirname "$(readlink "$0")")"
+CHDKPTP_DIR="$(dirname "$(readlink -f "$0")")"
 # set lua paths, double ; appends default
 export LUA_PATH="$CHDKPTP_DIR/lua/?.lua;;"
 export LUA_CPATH="$CHDKPTP_DIR/?.so;;"
--- misc/chdkptp_gui-dist.sh
+++ misc/chdkptp_gui-dist.sh
@@ -2,7 +2,7 @@
 # shell script wrapper for self contained GUI install
 CHDKPTP_EXE=chdkptp_gui
 # path where chdkptp is installed
-CHDKPTP_DIR="$(dirname "$(readlink "$0")")"
+CHDKPTP_DIR="$(dirname "$(readlink -f "$0")")"
 # LD_LIBRARY_PATH for shared libraries, assumed to be in lib sudir
 export LD_LIBRARY_PATH="$CHDKPTP_DIR/lib"
 # set lua paths, double ; appends default
