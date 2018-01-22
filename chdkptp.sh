c32e2db17a77b44ab6054adb7d0a9d1cd6baff17
reyalp <reyalp@2d6b39dd-413e-4bac-91f6-06b8ab484445>
2018-01-22 04:26:08 +0000

    replace chdkptp-sample.sh with wrapper that works with default locations
    git-svn-id: http://subversion.assembla.com/svn/chdkptp/trunk@824 2d6b39dd-413e-4bac-91f6-06b8ab484445

---
+++ chdkptp.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# shell script wrapper for builds in the source tree
+# chdkptp executable
+# if you want separate GUI and CLI executables, make a copy that uses _gui
+CHDKPTP_EXE=chdkptp
+# path where chdkptp is installed
+CHDKPTP_DIR="$(dirname "$(readlink -f "$0")")"
+# LD_LIBRARY_PATH for shared libraries, assumed to be in lib subdir
+# only needed for GUI in most cases, but should be harmless
+export LD_LIBRARY_PATH="$CHDKPTP_DIR/lib"
+# set lua paths, double ; appends default
+export LUA_PATH="$CHDKPTP_DIR/lua/?.lua;;"
+export LUA_CPATH="$CHDKPTP_DIR/?.so;;"
+"$CHDKPTP_DIR/$CHDKPTP_EXE" "$@"
